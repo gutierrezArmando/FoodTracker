4fea3470a00e18cf288b691a1bc8a11201d57f9b
gutierrezArmando <gutierrezjarr@gmail.com>
2018-09-04 15:33:41 -0700

    Seleccion de Imagen
    Se activa el boton de seleccion de imagen, y se coloca una imagen predeterminada.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     //MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton){
@@ -44,7 +45,36 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func selectImagePhotoLibrary(_ sender: Any) {
+        //Hide the keyboard
+        nameTextField.resignFirstResponder()
+        
+        //UIImagePrickerController is a view controller that lets a user pick a media from their photo library
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.sourceType = .photoLibrary
+        
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
+    //MARK: UIImagePcikerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //Dismiss the picker if the user canceled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //The info dictionary contains multiple rpresentations of the image, and this use the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
 
+        //Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
 
+        //Dismiss picker
+        dismiss(animated: true, completion: nil)
+    }
 }
-
